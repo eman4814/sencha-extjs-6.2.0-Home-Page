b796ed28653322f2ff1a8246e5bf83e1706ef61e
emansulaeman <eman4814@gmail.com>
2017-05-29 07:52:22 +0700

    first commit

---
+++ examples.js
@@ -0,0 +1,81 @@
+Ext = window.Ext || {};
+Ext.samples = {
+    samplesCatalog: [
+        {
+            "title": "My Products",
+            "items": [
+                {
+                    "text": "Futsal Booking System",
+                    "url": "products/futsal_booking_system.html",
+                    "icon": "webdesktop",
+                    "desc": "Sistem untuk mencatat pemesanan dan pembayaran lapangan futsal berbasis desktop, dibangun dengan konsep DAO menggunakan bahasa pemrograman java."
+                },
+                {
+                    "text": "Cloth Distribution System",
+                    "url": "products/cloth_distribution_system.html",
+                    "icon": "webdesktop",
+                    "desc": "Aplikasi berbasis desktop untuk management brand/distributor clothing, meliputi produksi, konsinyasi dan stock."
+                },
+                {
+                    "text": "Distro Retail",
+                    "url": "products/distro_retail.html",
+                    "icon": "webdesktop",
+                    "desc": "Aplikasi berbasis desktop untuk mencatat transaksi penjualan clothing pada distro."
+                },
+                {
+                    "text": "Resto Management",
+                    "url": "products/resto_management.html",
+                    "icon": "webdesktop",
+                    "desc": "Aplikasi berbasis desktop untuk management resto, meliputi pembelian, inventory, penjualan dan stock opname."
+                },
+                {
+                    "text": "Resto POS (Point of Sales)",
+                    "url": "products/resto_point_of_sales.html",
+                    "icon": "webdesktop",
+                    "desc": "Sistem ini akan dibagi menjadi dua modul yaitu Back Office dan Point of sales. Sistem Back Office akan dioperasikan di kantor produksi dan POS akan dioperasikan di outlet."
+                }
+            ]
+        },
+        {
+            "title": "About Me",
+            "items": [
+                {
+                    "text": "Profil",
+                    "url": "profil/profil.html",
+                    "icon": "rtl",
+                    "desc": "it is about my name, address, collage and hobby"
+                },
+                {
+                    "text": "Portofolio",
+                    "url": "portofolio/portofolio.html",
+                    "icon": "slider",
+                    "desc": "Overview my products"
+                },
+                {
+                    "text": "Work Experience",
+                    "url": "work/work_experience.html",
+                    "icon": "history",
+                    "desc": "Fides Jogja, UNIQ Jogja, Pragma Informatika, Icon+ PLN "
+                },
+                {
+                    "text": "Ability",
+                    "url": "ability/ability.html",
+                    "icon": "exec-dashboard",
+                    "desc": "Komunikatif, Pekerja keras, Teliti"
+                },
+                {
+                    "text": "Programing Skill",
+                    "url": "skills/skills.html",
+                    "icon": "nested-loading",
+                    "desc": "Java SE, Java ME, Spring Framework, Hibernate, Laravel, Sencha Extjs, Arduino, Android"
+                },
+                {
+                    "text": "Social Media",
+                    "url": "social_media/social_media.html",
+                    "icon": "feeds",
+                    "desc": "I active on Facebook, Instagram, LinkedIn, Github, E-mail"
+                }
+            ]
+        }
+    ]
+};
